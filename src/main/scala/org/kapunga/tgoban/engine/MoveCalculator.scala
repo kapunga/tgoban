8939d5b29b3c369f4e5d2a260a8c76285a278169
Paul J Thordarson <kapunga@gmail.com>
2014-08-14 09:14:33 -0400

    Added basic engine traits and classes.

---
+++ src/main/scala/org/kapunga/tgoban/engine/MoveCalculator.scala
@@ -0,0 +1,8 @@
+package org.kapunga.tgoban.engine
+
+/**
+ * Created by kapunga on 8/14/14.
+ */
+trait MoveCalculator {
+
+}
---
+++ src/main/scala/org/kapunga/tgoban/engine/OpinionMap.scala
@@ -0,0 +1,8 @@
+package org.kapunga.tgoban.engine
+
+import org.kapunga.tgoban.MetaBoard
+
+
+class OpinionMap(size: Int) extends MetaBoard[Float](size, 0.0) {
+
+}
